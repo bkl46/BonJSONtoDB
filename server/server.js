08fccad9e0adb4a6e1390a7f7d09a4b5cfbc79e2
bkl46 <bkl46@case.edu>
2025-04-11 17:19:59 -0400

    f update

--- server/server.js
+++ server/server.js
@@ -26,13 +26,13 @@ app.get('/entries', async (req, res) => {
   }
 });
 
-const aa = "INSERT INTO fribley (name, kcal, serving_size, total_fat, sat_fat, trans_fat, cholesterol, sodium, carbs, fiber, sugars, protein, vegetarian, vegan, made_without_gluten_containing_ingredients, balance, halal, farm_to_fork, humane, offered_often) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$20) ";
+const aa = "INSERT INTO fribley (name, kcal, serving_size, total_fat, sat_fat, trans_fat, cholesterol, sodium, carbs, fiber, sugars, protein, vegetarian, vegan, made_without_gluten_containing_ingredients, balance, halal, farm_to_fork, humane, offered_often, date) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$20,$21) ";
 // Add an entry
 app.post('/entries', async (req, res) => {
   try {
-    const { c1, c2, c3,c4,c5,c6,c7,c8,c9,c10,c11,c12,c13,c14,c15,c16,c17,c18,c19,c20 } = req.body;
+    const { c1, c2, c3,c4,c5,c6,c7,c8,c9,c10,c11,c12,c13,c14,c15,c16,c17,c18,c19,c20, c21 } = req.body;
 
-    const result = await pool.query(aa,[c1, c2, c3,c4,c5,c6,c7,c8,c9,c10,c11,c12,c13,c14,c15,c16,c17,c18,c19,c20 ]);
+    const result = await pool.query(aa,[c1, c2, c3,c4,c5,c6,c7,c8,c9,c10,c11,c12,c13,c14,c15,c16,c17,c18,c19,c20, c21 ]);
     res.json("hi");
     //res.json(result.rows[0]);
   } catch (err) {
